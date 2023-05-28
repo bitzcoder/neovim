a431f50b7203ef6f03d96f2200c51c8ae19647c2
asif <asif.nihl@pm.me>
2023-05-28 10:38:46 +0600

    Add separate config files for null-ls formatters

--- lua/user/plugins/null-ls.lua
+++ lua/user/plugins/null-ls.lua
@@ -25,32 +25,27 @@ return {
 
     local async_formatting = function(bufnr)
       bufnr = bufnr or vim.api.nvim_get_current_buf()
-      vim.lsp.buf_request(
-        bufnr,
-        "textDocument/formatting",
-        vim.lsp.util.make_formatting_params({}),
-        function(err, res, ctx)
-          if err then
-            local err_msg = type(err) == "string" and err or err.message
-            -- you can modify the log message / level (or ignore it completely)
-            vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-            return
-          end
-
-          -- don't apply results if buffer is unloaded or has been modified
-          if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-            return
-          end
-
-          if res then
-            local client = vim.lsp.get_client_by_id(ctx.client_id)
-            vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-            vim.api.nvim_buf_call(bufnr, function()
-              vim.cmd("silent noautocmd update")
-            end)
-          end
+      vim.lsp.buf_request(bufnr, "textDocument/formatting", vim.lsp.util.make_formatting_params({}), function(err, res, ctx)
+        if err then
+          local err_msg = type(err) == "string" and err or err.message
+          -- you can modify the log message / level (or ignore it completely)
+          vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+          return
         end
-      )
+
+        -- don't apply results if buffer is unloaded or has been modified
+        if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+          return
+        end
+
+        if res then
+          local client = vim.lsp.get_client_by_id(ctx.client_id)
+          vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+          vim.api.nvim_buf_call(bufnr, function()
+            vim.cmd("silent noautocmd update")
+          end)
+        end
+      end)
     end
 
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -64,11 +59,9 @@ return {
       debug = false,
       border = "rounded",
       sources = {
-        formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-        formatting.black.with({ extra_args = { "--fast", "--line-length=300", "--skip-string-normalization" } }),
-        formatting.stylua.with({
-          extra_args = { "--config-path", vim.fn.expand("~/.config/nvim/stylua.toml") },
-        }),
+        formatting.prettier.with({ extra_args = { "--config", vim.fn.expand("~/.config/nvim/utils/prettierrc.toml") } }),
+        formatting.black.with({ extra_args = { "--config", vim.fn.expand("~/.config/nvim/utils/black.toml") } }),
+        formatting.stylua.with({ extra_args = { "--config-path", vim.fn.expand("~/.config/nvim/utils/stylua.toml") } }),
         -- diagnostics.cspell.with({ filetypes = { "markdown", "txt", "text" } }),
       },
       -- Format on save
