d08119bc2015dbb94e1ac86a2ff6e22c8dffc4ae
asif <asif.nihl@pm.me>
2023-06-07 09:27:14 +0600

    Lazyload autocommands and keymaps

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
 require("user.options")
-require("user.keymaps")
 require("user.lazy")
-require("user.autocommands")
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("user.keymaps")
+    require("user.autocommands")
+  end,
+})
